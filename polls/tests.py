6046eebba9a6a408c9591858822fe424b2c31dab
Wen Chen <w1chen@yahoo.com>
2016-11-22 16:31:42 -0500

    fix coding style

--- polls/tests.py
+++ polls/tests.py
@@ -5,10 +5,11 @@ import datetime
 
 from django.utils import timezone
 
-from .models import Question, Choice
+from .models import Question
+
 
 class QuestionMethodTests(TestCase):
-    
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() should return False for questions whose
@@ -17,7 +18,7 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-        
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() should return False for questions whose
