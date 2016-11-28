6b7a54b3f2db368490f3ba6c87a7fef2e2ac4586
wjchen <wjchen@unallocated.barefruit.co.uk>
2016-11-27 21:58:36 -0500

    pylint fixes

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,5 @@
-from django.db import models
-
-# Create your models here.
 import datetime
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
--- polls/tests.py
+++ polls/tests.py
@@ -1,10 +1,6 @@
-from django.test import TestCase
-
-# Create your tests here.
 import datetime
-
+from django.test import TestCase
 from django.utils import timezone
-
 from .models import Question
 
 
